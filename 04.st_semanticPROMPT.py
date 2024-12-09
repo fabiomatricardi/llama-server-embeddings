f53d861c6d0b5be290b3ce6ff2912775c17e4c83
Fabio Matricardi <fabio.matricardi@gmail.com>
2024-12-09 22:45:14 +0800

    Add files via upload

--- 04.st_semanticPROMPT.py
+++ 04.st_semanticPROMPT.py
@@ -18,6 +18,26 @@ import configparser
 import tiktoken
 from sentence_transformers.util import pytorch_cos_sim, cos_sim, dot_score
 from time import sleep
+import configparser
+
+def read_config():
+    # Create a ConfigParser object
+    config = configparser.ConfigParser()
+    # Read the configuration file
+    config.read('tiny-vicuna-1b.ini',encoding='utf-8')
+    # Access values from the configuration file
+    NCTX = config.getint('Model', 'NCTX')
+    modelname = config.get('Model', 'name')
+    modelfile = config.get('Model', 'file')
+    STOPS = config.get('Model', 'STOPS')
+    myheader = config.get('UI', 'myheader')
+    cursor = config.get('UI', 'cursor')
+    av_us = config.get('UI', 'av_us')
+    av_ass = config.get('UI', 'av_ass')
+    # Return a dictionary with the retrieved value
+    return NCTX,modelname,modelfile,STOPS,myheader,cursor,av_us,av_ass
+
+NCTX,modelname,modelfile,STOPS,myheader,cursor,av_us,av_ass = read_config()
 
 def countTokens(text):
     """
@@ -36,13 +56,14 @@ embeddingname = 'gte-small_fp16.gguf'
 if "firstrun" not in st.session_state:
     st.session_state.firstrun = 0
 
-standard = """One of the things everybody in the West knows about China is that it is not a democracy, and is instead a regime run with an iron fist by a single entity, the Chinese Communist Party, whose leadership rarely acts transparently, running the country without the need for primary elections, alternative candidacies, etc.
+standard1 = """One of the things everybody in the West knows about China is that it is not a democracy, and is instead a regime run with an iron fist by a single entity, the Chinese Communist Party, whose leadership rarely acts transparently, running the country without the need for primary elections, alternative candidacies, etc.
 In general, those of us who live in democracies, with relatively transparent electoral processes, tend to consider the Chinese system undesirable, little more than a dictatorship where people have no say in who governs them.
 That said, among the “advantages” of the Chinese system is that because the leadership never has to put its legitimacy to the vote, it can carry out very long-term planning in the knowledge that another administration isn’t going to come along and change those plans.
 Obviously, I put “advantages” in quotation marks because, as democrats, most of my readers would never be willing to sacrifice their freedom for greater planning, but there is no doubt that China, since its system works like this and its population seems to have accepted it for generations, intends to turn this into a comparative advantage, the term used in business when analyzing companies.
 It turns out that China’s capacity for long-term planning is achieving something unheard of in the West: it seems the country reached peak carbon dioxide and greenhouse gas emissions in 2023, and that the figures for 2024, driven above all by a determined increase in the installation of renewable energies, are not only lower, but apparently going to mark a turning point.
 China and India were until recently the planet’s biggest polluters, but they now offer a model for energy transition (there is still a long way to go; but we are talking about models, not a done deal).
 It could soon be the case that the so-called developing countries will be showing the West the way forward."""
+standard = """Large language models (LLMs) have shown great capabilities but also have flaws. These include the vice of producing hallucinations or the presence of outdated content. This has led to the emergence of a new paradigm called retrieval augmented generation (RAG). Previous LLMs had a limited context length (usually no more than 4096), which significantly limited the context that could be entered into the prompt. This meant time-consuming and laborious optimization work to find the appropriate context. In fact, one of the sore points of RAG is chunking and the need to choose a suitable chunking strategy for one’s data. Over the years intensive research has been devoted to extending the context length of today’s LLMs precisely to reduce this problem and be able to provide more context to the model."""
 #rewriting AVATARS  👷🐦  🥶🌀
 av_us = 'user.png'  #or "🦖"  #A single emoji, e.g. "🧑‍💻", "🤖", "🦖". Shortcodes are not supported.
 av_ass = 'assistant.png'
@@ -125,26 +146,12 @@ with st.container():
         user_context = st.text_area(label='Context:', height=200,disabled=not(rag_required))
         btn_inference = st.button(label='AI reply', type='primary')
     with c2:
+        st.markdown(f"### TinyVicuna reply")
         with st.container(border=True):
             reply = st.empty()    
-if btn_inference:
-        status.markdown('STATUS MESSAGES:   checking the query relevance with the context...') 
-        sleep(2)
-        status.markdown('STATUS MESSAGES:   relevance Score Cosine 0.7760...')
-        sleep(1)
-        status.markdown('STATUS MESSAGES:   Query and context relevant Question is answerable...')
-        sleep(1)
-        res = embed_QUERY(user_input,user_context,embeddings)
-        st.toast("Let's do it", icon='😍') 
-        sleep(1)
-        results.markdown(f'Similarity score: **{res[0][0]:.5f}**')
-        reply.markdown(standard)
-        #results.write(res.data[1].embedding)
-
-
-st.write('\n\n\n\n')
-st.divider()
-st.markdown("""Other possible similarity scores:
+        st.markdown(f"###### *About similarity score threshold*")
+        with st.popover("Additional info about simScore"):
+                st.markdown("""Other possible similarity scores:
 For some reasons are giving back the same numners...
 ```            
 # compute similarity (3 methods)
@@ -170,3 +177,74 @@ print(hits)
 - @st.cache_resource does not work with OpenAI client instantiation
 - @st.cache_resource cannot call another function under st.cache (for ecample `embed_QUERY(question,context,client)`)                               
 """)
+if btn_inference:
+        if rag_required:
+            sleep(1)
+            st.toast("Let's do it", icon='😍') 
+            status.markdown('STATUS MESSAGES:   checking the query relevance with the context...') 
+            res = embed_QUERY(user_input,user_context,embeddings)
+            simscore = float(res[0][0])
+            if simscore>=0.7760:
+                sleep(2)
+                status.markdown('STATUS MESSAGES:   relevance Score Cosine threshold >= 0.7760...')
+                sleep(1)
+                status.markdown('STATUS MESSAGES:   Query and context relevant Question is **:green[answerable...]**')
+                results.markdown(f'Similarity score: **:green[{simscore:.5f}]**')
+                sleep(1)
+                status.markdown('STATUS MESSAGES:   **:green[Generating reply...]**')
+                prompt = f"""A chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions. USER: Reply to the question only using the provided context.
+[context]{user_context}[end of context]
+
+Question: {user_input}
+ASSISTANT:"""
+                full_response = ''
+                for items in llm.completions.create(
+                                prompt =prompt,
+                                model=modelname,
+                                temperature=0.15,
+                                presence_penalty=1.35,
+                                stop=STOPS,
+                                max_tokens=500,              
+                                stream=True):
+                    full_response += items.content
+                    reply.markdown(full_response + cursor)
+                reply.markdown(full_response)                 
+            else:
+                sleep(2)
+                full_response = ''
+                status.markdown('STATUS MESSAGES:   relevance Score Cosine threshold >= 0.7760...')
+                sleep(1)
+                status.markdown('STATUS MESSAGES:   Query and context relevant Question is **:red[UNANSWERABLE...]**')            
+                results.markdown(f'Similarity score: **:red[{simscore:.5f}]**')
+                mymessage = "I DON'T KNOW. Based on the given context the question is UNANSWERABLE"
+                for items in mymessage:
+                    full_response += items
+                    reply.markdown(full_response + cursor)
+                    sleep(0.02)
+                reply.markdown(full_response)
+                #results.write(res.data[1].embedding)
+        else:
+            sleep(1)
+            st.toast("Let's do it", icon='😍')             
+            status.markdown('STATUS MESSAGES:   **:green[Generating reply...]**')
+            prompt = f"""A chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions. USER: Reply to the question only using the provided context.
+[context]{user_context}[end of context]
+
+Question: {user_input}
+ASSISTANT:"""
+            full_response = ''
+            for items in llm.completions.create(
+                            prompt =prompt,
+                            model=modelname,
+                            temperature=0.15,
+                            presence_penalty=1.35,
+                            stop=STOPS,
+                            max_tokens=500,              
+                            stream=True):
+                full_response += items.content
+                reply.markdown(full_response + cursor)
+            reply.markdown(full_response)   
+            
+st.write('\n\n\n\n')
+st.divider()
+
---
+++ runservers.bat
@@ -0,0 +1,4 @@
+start .\llama.cpp\llama-server.exe --embeddings -m D:\PortableLLMs\2024.TinyVicuna1B\llama.cpp\model\gte-small_fp16.gguf -c 1024 --port 8002
+
+start .\llama.cpp\llama-server.exe -m D:\PortableLLMs\2024.TinyVicuna1B\llama.cpp\model\tiny-vicuna-1b.q6_k.gguf -c 2048 --port 8001
+PAUSE
